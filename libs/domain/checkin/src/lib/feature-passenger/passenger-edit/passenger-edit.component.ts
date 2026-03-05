f6e9497a15944b5dc1203ad0b1d86ccd7d27867f
Michael Egger-Zikes <23002661+mikezks@users.noreply.github.com>
2026-03-05 13:17:02 +0100

    feat: signal forms w/ validators

--- libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
+++ libs/domain/checkin/src/lib/feature-passenger/passenger-edit/passenger-edit.component.ts
@@ -1,9 +1,25 @@
 import { httpResource } from '@angular/common/http';
-import { Component, input, numberAttribute, signal } from '@angular/core';
+import { Component, input, numberAttribute } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { form, FormField } from '@angular/forms/signals';
+import { form, FormField, required, schema, validate } from '@angular/forms/signals';
 import { RouterLink } from '@angular/router';
 import { initialPassenger, Passenger } from '../../logic-passenger/model/passenger';
+import { ClickLocal } from "@flight-demo/shared/navigation";
+
+// Step 3: Field Logic: Validators, Readonly, Disabled, Field Metadata
+export const passengerSchema = schema<Passenger>(passengerPath => {
+  required(passengerPath.firstName, {
+    message: 'The Firstname is mandatory.'
+  });
+  validate(passengerPath.name, ({ value }) =>
+    ['Sorglos', 'Müller', 'Schmidt'].includes(value())
+      ? null
+      : {
+        kind: 'forbiddenLastname',
+        message: 'This Lastname is not allowed.'
+      }
+  );
+});
 
 
 @Component({
@@ -11,19 +27,23 @@ import { initialPassenger, Passenger } from '../../logic-passenger/model/passeng
   imports: [
     ReactiveFormsModule,
     RouterLink,
-    FormField
+    // Step 4: UI Control -> Directive for Template Binding
+    FormField,
+    ClickLocal
 ],
   templateUrl: './passenger-edit.component.html'
 })
 export class PassengerEditComponent {
   readonly id = input(0, { transform: numberAttribute });
 
+  // Step 1: Data Model -> Writable Signal, Resource Value
   protected readonly passengerResource = httpResource<Passenger>(() => ({
     url: 'https://demo.angulararchitects.io/api/passenger',
     params: { id: this.id() }
   }), { defaultValue: initialPassenger });
 
-  protected readonly editForm = form(this.passengerResource.value);
+  // Step 2: Field State -> valid, dirty, touched, value, etc. 
+  protected readonly editForm = form(this.passengerResource.value, passengerSchema);
   
   protected save(): void {
     console.log({
